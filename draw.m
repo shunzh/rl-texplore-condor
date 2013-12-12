704e07de8dcb1fc7ef6bde71ced28904d943fdcd
Shun Zhang <menie482@gmail.com>
2013-12-12 11:31:46 -0600

    changes in drawing code. selfsepa 3000 fails

--- draw.m
+++ draw.m
@@ -8,7 +8,7 @@ function retval = confidence(m)
 	retval = 1.96 * std(m, [], 2) / sqrt(size(m, 2));
 endfunction
 
-function retval = readResult(name)
+function retval = readResult(name, range)
 	result = [];
 
 	num = 0;
@@ -22,7 +22,7 @@ function retval = readResult(name)
 		endif
 
 		rawData = dlmread(fileName);
-		data = rawData(44: 10 : 1041, 1);
+		data = rawData(range, 1);
 
 		result = [result, data];
 	endfor
@@ -33,24 +33,19 @@ endfunction
 % =============== main ===============
 % file name and # of files
 % prosumebly, [$1.0, $1.1, ..., $1.($2-1)]
-result = readResult("ast_gen_c45/gen");
 x = 0 : 10 : 990;
+
+result = readResult("ast_gen_c45/gen", 44: 10 : 1041);
 errorbar(x, result(:, 1), result(:, 2), "#r");
 hold on;
 
-result = readResult("ast_gen_selfsepa/out");
-x = 0 : 10 : 990;
-errorbar(x, result(:, 1), result(:, 2), "#y");
-hold on;
-
-result = readResult("ast_spe/out");
-x = 0 : 10 : 990;
+result = readResult("ast_spe/out", 44: 10 : 1041);
 errorbar(x, result(:, 1), result(:, 2), "#b");
 hold on;
 
-result = readResult("ast_selfandsamerow/out");
-x = 0 : 10 : 990;
+result = readResult("ast_selfandsamerow/out", 44: 10 : 1041);
 errorbar(x, result(:, 1), result(:, 2), "#m");
+hold on;
 
 % decorate graph
 %axis([0 1000 -600 0]);
